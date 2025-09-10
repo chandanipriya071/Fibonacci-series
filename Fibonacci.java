57828558f9ad4951fe0654fc6a0e430ebddd2371
Chandani Kumari <chandanip54321@gmail.com>
2025-09-10 13:20:07 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+class fibonacci{
+  public static void main(String[]abc){
+    int firstTerm=0;
+    int secondTerm=1;
+    int nextTerm;
+    System.out.println("firstTerm");
+     System.out.println("secondTerm");
+    for(int i=1;i<=10;i++){
+      nextTerm=firstTerm+secondTerm;
+      System.out.println(nextTerm);
+      firstTerm=secondTerm;
+      secondTerm=nextTerm;
+    }
+  }
+}
+    
